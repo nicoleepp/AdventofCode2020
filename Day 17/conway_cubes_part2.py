673ae0c8b071c14d3dce9c225ae69757eedfa1c9
Nicole Epp <nicole.epp@invenia.ca>
2020-12-17 18:16:11 -0600

    Add Day 17

---
+++ Day 17/conway_cubes_part2.py
@@ -0,0 +1,71 @@
+ACTIVE_STATE = "#"
+INACTIVE_STATE = "."
+NUM_CYCLES = 6
+
+
+def num_active_neighbours(cubes, cube):
+    x, y, z, w = cube
+    num_active = 0
+
+    for curr_x in (x-1, x, x+1):
+        for curr_y in (y-1, y, y+1):
+            for curr_z in (z-1, z, z+1):
+                for curr_w in (w-1, w, w+1):
+
+                    if curr_x == x and curr_y == y and curr_z == z and curr_w == w:
+                        continue
+
+                    state = cubes.get((curr_x, curr_y, curr_z, curr_w), None)
+                    if state is not None and state == ACTIVE_STATE:
+                        num_active += 1
+
+    return num_active
+
+
+def new_grid(cubes):
+    new_cubes = {}
+    for (x, y, z, w) in cubes:
+        for curr_x in (x-1, x, x+1):
+            for curr_y in (y-1, y, y+1):
+                for curr_z in (z-1, z, z+1):
+                    for curr_w in (w-1, w, w+1):
+
+                        state = cubes.get((curr_x, curr_y, curr_z, curr_w), None)
+                        if state is None:
+                            new_cubes[(curr_x, curr_y, curr_z, curr_w)] = INACTIVE_STATE
+                        else:
+                            new_cubes[(curr_x, curr_y, curr_z, curr_w)] = state
+    return new_cubes
+
+
+if __name__ == "__main__":
+    with open("Day 17/input.txt", "r") as f:
+        input = f.read().splitlines()
+
+    cubes = {}
+    for y in range(len(input)):
+        for x in range(len(input[y])):
+            cubes[(x, y, 0, 0)] = input[y][x]
+
+
+    for _ in range(NUM_CYCLES):
+        new_cubes = new_grid(cubes)
+
+        for cube in new_cubes:
+            num_neighbours = num_active_neighbours(cubes, cube)
+
+            if new_cubes[cube] == ACTIVE_STATE:
+                if num_neighbours not in (2, 3):
+                    new_cubes[cube] = INACTIVE_STATE
+            else:
+                if num_neighbours == 3:
+                    new_cubes[cube] = ACTIVE_STATE
+
+        cubes = new_cubes
+
+    num_active = 0
+    for cube in cubes:
+        if cubes[cube] == ACTIVE_STATE:
+            num_active += 1
+
+    print(num_active)
